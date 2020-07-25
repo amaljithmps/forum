e9ec63131b3fd90dc5cbb19d5c12e5669013bca0
Amaljith M P S <62708925+amaljithmps@users.noreply.github.com>
2020-07-25 18:51:36 +0530

    Authorization-->

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const { urlencoded } = require('express');
 require('dotenv/config');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 //database
 mongoose.connect(process.env.DB_connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
@@ -12,12 +14,43 @@ mongoose.connect(process.env.DB_connection,{ useNewUrlParser: true, useUnifiedTo
 
 //Middleware
 
-app.use(express.urlencoded({extended: true}));
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //start server
 app.listen(process.env.PORT || 3000, () => console.log("Server Connection Established"));
+
+
+//authorization
+function auth(req, res, next){
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    } else {
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var username = auth[0];
+        var password = auth[1];
+        
+        if(username === 'admin' && password === 'password') {
+            next();
+        } else {
+            var err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+
+app.use(auth);
 
 //models
 const ForumModel = require('./models/forumModel');
