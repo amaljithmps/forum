8941ed1d94cb6c26adf30c771e0a0aac6c79cee6
Amaljith M P S <62708925+amaljithmps@users.noreply.github.com>
2020-07-30 14:54:42 +0530

    imageFile upload

---
+++ controllers/uploadControl.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const authenticate = require('../authenticate');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, 'public/images');        
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+const imageFileFilter = (req, file, cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(new Error('Upload an image file!'), false);
+    }
+    cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter});
+
+const uploadRouter = express.Router();
+uploadRouter.use(bodyParser.json());
+
+uploadRouter.post('/',authenticate.verifyUser, upload.single('imageFile'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json(req.file);
+});
+
+module.exports = uploadRouter;
--- server.js
+++ server.js
@@ -9,12 +9,8 @@ const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const passport = require('passport');
-
-
 const config = require('./config');
 const authenticate = require('./authenticate');
-
-
 
 //database
 mongoose.connect(process.env.DB_connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
@@ -28,36 +24,22 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-
 //models
 const ForumModel = require('./models/forumModel');
 
 //controllers
 const forumControl = require('./controllers/forumControl');
+const uploadRouter = require('./controllers/uploadControl');
 const userRouter = require('./controllers/user');
 
 //start server
 app.listen(process.env.PORT || 3000, () => console.log("Server Connection Established"));
 
 
-//app.use(cookieParser('12345-67890-09876-54321'));
-/*app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));*/
-
 app.use(passport.initialize());
-//app.use(passport.session());
 
+app.use('/', userRouter);
 
-app.use('/',userRouter);
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-forumControl(app,ForumModel);
-
-
+forumControl(app, ForumModel);
+app.use('/imageUpload', uploadRouter);
